5389199593152df86c1e6108946a4582fee30c5f
Antti Keränen <detegr@rbx.email>
2023-04-25 16:50:24 +0300

    Fix rpath on MacOS

--- lsp/build.rs
+++ lsp/build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+
+    #[cfg(all(target_family = "unix", not(target_os = "macos")))]
     println!("cargo:rustc-link-arg=-Wl,-rpath=$ORIGIN");
 }
